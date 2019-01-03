c90c8ed58f5c903917140775a447f8125e084539
ColMugX <colmugx@qq.com>
2019-01-03 09:21:59 +0800

    update: inject

--- src/rabbit/decorator.ts
+++ src/rabbit/decorator.ts
@@ -7,17 +7,21 @@ interface DefaultProp {
   dispatch: Dispatch
 }
 export const inject = (namespace: string, connect: Connect, store?: RStore) => {
-
-  const mapStateToProps = state => namespace in state ? state[namespace] : state
-  const effects =
-    (store ? Object.keys(store.effects) : [])
-    .filter(item => item.indexOf(`${namespace}/`) === 0)
+  const mapStateToProps = state =>
+    namespace in state ? state[namespace] : state
+  const effects = (store ? Object.keys(store.effects) : []).filter(
+    item => item.indexOf(`${namespace}/`) === 0
+  )
 
   const mapDispatchToProps = (dispatch: Dispatch) => {
     return effects.reduce((com, item) => {
       const fnName = item.split('/')[1]
       const fn = (rest: object | string) =>
-        dispatch(typeof rest === 'string' ? ({ type: item, payload: rest }) : ({ type: item, ...rest }))
+        dispatch(
+          typeof rest === 'string'
+            ? { type: item, payload: rest }
+            : { type: item, ...rest }
+        )
       return {
         ...com,
         [fnName]: fn
@@ -28,10 +32,13 @@ export const inject = (namespace: string, connect: Connect, store?: RStore) => {
   return Component => {
     class ReactComponent<P = {}, S = {}> extends Component<P & DefaultProp, S> {
       public static displayName = Component.name
-      constructor (props) {
+      constructor(props) {
         super(props)
       }
     }
-    return connect(mapStateToProps, mapDispatchToProps)(ReactComponent as ComponentType)
+    return connect(
+      mapStateToProps,
+      mapDispatchToProps
+    )(ReactComponent as ComponentType)
   }
 }
