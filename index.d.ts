1634caf162401997f69539efba1f49693299481a
ColMugX <colmugx@qq.com>
2019-01-07 16:11:25 +0800

    update: d.ts, no dependence.

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,30 @@
-import {
-  Model,
-  EffectsMapObject
-} from './src/core/model'
-import { Store, ReducersMapObject } from 'redux'
-import { RStore } from './src/core/rabbit'
+import { AnyAction, Dispatch, ReducersMapObject, Store } from 'redux'
+import { Component } from 'react';
 
-export function inject(namespace: string, connect: any, store?: RStore): any
-export default function rabbit(model: Model[]): RStore
-
-export {
-  Model,
-  EffectsMapObject,
-  ReducersMapObject
+interface EffectsType {
+  dispatch: Dispatch<AnyAction>
+  select: (state: any) => void
 }
+export interface EffectsMapObject {
+  [key: string]: (action: AnyAction, effect: EffectsType) => void
+}
+
+export interface Model {
+  name: string
+  state?: any
+  reducers?: ReducersMapObject
+  effects?: EffectsMapObject
+}
+
+export interface RStore extends Store {
+  effects: any
+}
+
+export interface Modules {
+  [key: string]: Model
+}
+interface DefaultProp {
+  dispatch: Dispatch
+}
+export default function rabbit(model: Model[]): RStore
+export function inject(namespace: string, connect: any, store?: RStore): <T extends {new(...args: any[]): {}}>(constructor: T) => T
